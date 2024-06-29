1f646c20df3aa8f8505a087272b17c9faef2ecfd
emamjion <emamjion@gmail.com>
2024-06-29 23:04:07 +0600

    project initialized and setup completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+// Components
+import Nav from "./Nav";
+// Shadcn button
+import { Button } from "./ui/button";
+
+const Header = () => {
+    return (
+        <header className="py-8 xl:py-12 text-white">
+            <div className="container max-auto flex justify-between items-center">
+                <Link href='/'>
+                    <h1 className="font-semibold text-4xl">Emam Jion<span className="text-accent">.</span></h1>
+                </Link>
+                
+                
+                {/* Desktop Nav and download resume button */}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav/>
+                    <Link href=''>
+                        <Button>Download Resume</Button>
+                    </Link>
+                </div>
+
+                {/* Mobile Nav */}
+                <div className="xl:hidden">Mobile Nav</div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
