4658ae30625aef4e229f24206e8df2c288f5f433
emamjion <emamjion@gmail.com>
2024-06-30 10:43:22 +0600

    about experience section design finished

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -66,6 +66,21 @@ const experience = {
             position: 'Frontend Designer',
             duration: '02/2024 - 05/2024'
         },
+        {
+            company: 'bdCalling IT Ltd.',
+            position: 'Frontend Designer',
+            duration: '02/2024 - 05/2024'
+        },
+        {
+            company: 'bdCalling IT Ltd.',
+            position: 'Frontend Designer',
+            duration: '02/2024 - 05/2024'
+        },
+        {
+            company: 'bdCalling IT Ltd.',
+            position: 'Frontend Designer',
+            duration: '02/2024 - 05/2024'
+        }
     ]
 }
 
@@ -178,7 +193,7 @@ const skills = {
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 // import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
-// import { ScrollArea } from '@/components/ui/scroll-area';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { motion } from 'framer-motion';
 
 
@@ -210,6 +225,45 @@ const About = () => {
                             {/* About me */}
                             <TabsContent value='about' className='w-full'>
                                 about me
+                            </TabsContent>
+
+
+
+                            {/* Skills */}
+                            <TabsContent value='skills' className='w-full'>
+                                Skills
+                            </TabsContent>
+
+
+
+                            {/* Experience */}
+                            <TabsContent value='experience' className='w-full'>
+                                <div className='flex flex-col gap-[30px] text-center xl:text-left'>
+                                    <h3 className='text-4xl font-semibold'>{experience.title}</h3>
+                                    <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{experience.description}</p>
+                                    <ScrollArea className='h-[400px]'>
+                                        <ul className='grid grid-cols-1 lg:grid-cols-2 gap-[30px]'>
+                                            {experience.items.map((item, index) => {
+                                                return <li key={index} className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'>
+                                                    <span className='text-accent'>{item.duration}</span>
+                                                    <h3 className='text-xl max-w-[260px] min-h-[60px] text-center lg:text-left'>{item.position}</h3>
+                                                    <div className='flex items-center gap-3'>
+                                                        {/* dot */}
+                                                        <span className='size-[6px] rounded-full bg-accent'></span>
+                                                        <p className='text-white/60'>{item.company}</p>
+                                                    </div>
+                                                </li>
+                                            })}
+                                        </ul>
+                                    </ScrollArea>
+                                </div>
+                            </TabsContent>
+
+
+
+                            {/* Education */}
+                            <TabsContent value='education' className='w-full'>
+                                Education
                             </TabsContent>
                         </div>
                     </Tabs>
